6430b13f3ec15242f28a3a1e7ca021354788bd45
branfts <support@june07.com>
2024-07-22 15:24:42 -0700

    Updated by A10x Service

---
+++ public/u/alabama.js
@@ -0,0 +1,16 @@
+
+const links = [
+    {
+        "url": "https://b.com",
+        "title": "b title",
+        "subtitle": "b subtitle"
+    },
+    {
+        "url": "https://c.com"
+    },
+    {
+        "url": "https://a.com"
+    }
+];
+const rules = [];
+export { links, rules };
